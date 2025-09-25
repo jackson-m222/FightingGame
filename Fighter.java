0ed62fc344ee95e3fc1f38edbf000517d19410ff
Jackson McFadyen <mcfadyenj@xavier.edu>
2025-09-25 16:20:07 -0400

    B-Level Fixes

--- Fighter.java
+++ Fighter.java
@@ -1,8 +1,3 @@
-/**
- * This class makes the fighters and their stats
- * The stats both the fighters get is a name, health pool and maximum damage and heal output
- * It also makes amount healed and damage random based on the max
- */
 import java.util.Random;
 
 public class Fighter {
@@ -10,14 +5,9 @@ public class Fighter {
     private String name;
     private int healthPoints;
     private int attackDamage;
-    private int damageDealt;
     private Random rng;
-    private int healAmount;
-    private int healthHealed;
-
-    /**
-     * Constructor that creates the fighter and gives it it's stats
-     */
+    private int healAmount;	
+    
     public Fighter() {
         name = "Bobby";
         healthPoints = 100;
@@ -26,13 +16,6 @@ public class Fighter {
         healAmount = 15;
     }
 
-    /**
-     * Method that creates the 2nd fighter in the main class
-     * @param name1 Name of the fighter
-     * @param health1 Health of the fighter
-     * @param dmg1 Maximum damage output of the fighter
-     * @param heal1 Maximum heal input of the fighter
-     */
     public Fighter(String name1, int health1, int dmg1, int heal1) {
         name = name1;
         healthPoints = health1;
@@ -57,24 +40,15 @@ public class Fighter {
         return healAmount;
     }
 
-    /**
-     * Method that lets the fighters deal damage
-     * Damage is based on a random number 1 through their maximum damage output
-     * Subtracts the damage they dealt from their enemy's health
-     * @return
-     */
+
     public int dealDamage() {
-        damageDealt = rng.nextInt(attackDamage) + 1;
+        int damageDealt = rng.nextInt(attackDamage) + 1;
         System.out.println(" ");
         System.out.println(name + " deals " + damageDealt + " damage.");
         return damageDealt;
     }
 
-    /**
-     * Method that subtracts the damage the enemy afflicted from the fighter's health
-     * Also prints how much health they have remaining after the attack
-     * @param amount
-     */
+
     public void takeDamage(int amount) {
         healthPoints = healthPoints - amount;
         System.out.println(" ");
@@ -82,23 +56,15 @@ public class Fighter {
         System.out.println(" ");
     }
 
-    /**
-     * Method that lets the fighter heal rather than attacking
-     * Health healed is based on a random number from 1 through the maximum health input
-     * Adds the health healed to their own health pool.
-     * @return
-     */
-    public int heal() {
-        healthHealed = rng.nextInt(healAmount) + 1;
+
+    public void heal() {
+        int healthHealed = rng.nextInt(healAmount) + 1;
         healthPoints = healthPoints + healthHealed;
         System.out.println(name + " healed " + healthHealed + " health. Now has " + healthPoints + " health");
         System.out.println(" ");
-        return healthHealed;
     }
 
-    /**
-     * Method that announces the fighters stats when the game begins.
-     */
+
     public String toString() {
         return name + " has " + healthPoints + " health, can deal 30 damage, and can heal 15.";
     }
